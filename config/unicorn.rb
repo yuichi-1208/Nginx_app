a88bd3436cfe8d632ccf8e9a23bd430a99aa49b2
yuichi oyama <yuichi19991208@gmail.com>
2020-10-24 20:10:06 +0900

    puma.rbを消してNginxの設定を記述した

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,9 @@
-# worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-# timeout 15
-# preload_app true
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+worker_processes 4
+listen 'unix:///tmp/nginx.socket', backlog: 1024
 
-# listen '/Users/oyamayuichi/workspace/Nginx_app/tmp/unicorn.sock'
-# pid    '/Users/oyamayuichi/workspace/Nginx_app/tmp/unicorn.pid'
-
-# before_fork do |server, worker|
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-#     Process.kill 'QUIT', Process.pid
-#   end
-
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.connection.disconnect!
-# end
-
-# after_fork do |server, worker|
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-#   end
-
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.establish_connection
-# end
-
-# stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-# stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+before_fork do |server,worker|
+    FileUtils.touch('/tmp/app-initialized')
+end
